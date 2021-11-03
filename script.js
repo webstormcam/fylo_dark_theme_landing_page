4f9a33a9fad52d6d0115fd1d21f1283ccae184ae
Cameron Chandler <webstormcam@gmail.com>
2021-11-03 08:34:41 -0400

    Made the JS do all the color changes

--- script.js
+++ script.js
@@ -6,8 +6,12 @@ greenLink.addEventListener('mouseout', mouseOut);
 
 function mouseOver(){
 greenIcon.src = 'imgs/icon-arrow-white.svg';
+greenLink.style.borderBottom = '1px solid #fff'
+greenLink.style.color ='#fff'
 }
 
 function mouseOut(){
     greenIcon.src = 'imgs/icon-arrow.svg';
+    greenLink.style.borderBottom = '1px solid #62E0D9'
+greenLink.style.color ='#62E0D9'
 }
